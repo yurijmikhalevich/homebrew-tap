2706afbb1119a48fb7697a32e38036278b734b1f
Yurij Mikhalevich <yurij@mikhalevi.ch>
2024-12-17 14:17:40 +0400

    fix torchvision tests

--- Formula/torchvision-python312@0.20.1.rb
+++ Formula/torchvision-python312@0.20.1.rb
@@ -6,7 +6,7 @@ class TorchvisionPython312AT0201 < Formula
   url "https://github.com/pytorch/vision/archive/refs/tags/v0.20.1.tar.gz"
   sha256 "7e08c7f56e2c89859310e53d898f72bccc4987cd83e08cfd6303513da15a9e71"
   license "BSD-3-Clause"
-  revision 4
+  revision 5
 
   keg_only "it conflicts with the torchvision formula"
 
@@ -81,6 +81,7 @@ class TorchvisionPython312AT0201 < Formula
     system ENV.cxx, "-std=c++17", "test.cpp", "-o", "test", *openmp_flags,
                     "-I#{pytorch.opt_include}",
                     "-I#{pytorch.opt_include}/torch/csrc/api/include",
+                    "-I#{include}",
                     "-L#{pytorch.opt_lib}", "-ltorch", "-ltorch_cpu", "-lc10",
                     "-L#{lib}", *("-Wl,--no-as-needed" if OS.linux?), "-ltorchvision"
 
