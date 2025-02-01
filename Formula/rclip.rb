08515f6d4737363650eea9d05d663011da15b389
Yurij Mikhalevich <yurij@mikhalevi.ch>
2025-02-01 21:23:01 +0400

    try fixing Cython

--- Formula/rclip.rb
+++ Formula/rclip.rb
@@ -7,10 +7,10 @@ class Rclip < Formula
   sha256 "7d4a58c637e0368de0767cb0539b6abbd3466da5c69e851e3a03cb473ed5c78e"
   license "MIT"
 
-  depends_on "cython" => :build # for rawpy
   depends_on "libraw" => :build # for rawpy
   depends_on "rust" => :build # for safetensors
   depends_on "certifi"
+  depends_on "cython" # for rawpy
   depends_on "libyaml"
   depends_on "numpy"
   depends_on "pillow"
@@ -135,6 +135,8 @@ class Rclip < Formula
   end
 
   def install
+    ENV.prepend_path "PYTHONPATH", Formula["cython"].opt_libexec/Language::Python.site_packages("python3.12")
+
     virtualenv_install_with_resources
 
     # link dependent virtualenvs to this one
